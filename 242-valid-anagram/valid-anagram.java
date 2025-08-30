a9a1b8386735a5f5390159588bc2ff797ad1bbae
RUTUJA MAVKAR <141482973+RutujaMavkar1204@users.noreply.github.com>
2025-08-30 19:11:10 +0530

    Time: 457 ms (6.08%) | Memory: 46 MB (6.66%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character, Integer> map=new HashMap<>();
+        if(s.length()!=t.length()){
+            return false;
+        }
+        int i=0;
+        while(i<s.length()){
+            if(map.containsKey(s.charAt(i))){
+                map.put(s.charAt(i),map.get(s.charAt(i))+1);
+            }
+            else{
+                map.put(s.charAt(i),1);
+            }
+            i++;
+        }
+        int j=0;
+        while(j<t.length()){
+           if(map.containsKey(t.charAt(j))){
+                map.put(t.charAt(j),map.get(t.charAt(j))-1);
+                System.out.println(map);
+            }
+            else{
+                return false;
+            }j++;
+        }
+        
+        int k=0;
+        while(k<s.length()){
+            if(map.get(s.charAt(k))!=0){
+                 System.out.println("map");
+                return false;
+            }
+            k++;
+        }
+        
+        return true;
+    }
+}
